f7e56b38327007e11f9558793e58fbd84ee01aeb
MARIAVITORIASANTOSROZARIO <166419969+MARIAVITORIASANTOSROZARIO@users.noreply.github.com>
2024-11-04 11:58:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'HTML é uma linguagem de marcação utilizada na construção de páginas na Web.'
+)
+
+criaCartao(
+    'Filme',
+    'Qual o filme com maior bilheteria do mundo',
+    'Avatar'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual ano foi lançado Minecraft?',
+    '2011'
+)
+
+criaCartao(
+    'Programação' ,
+    'Oque é HTML?' ,
+    ' HTML é uma linguagem de marcação ultilizada ma construção de páginas na web.'
+)
+criaCartao(
+    'musculação' ,
+    'Oque é creatina?' ,
+    ' A creatinina é um produto da degradação da fosfocreatina no músculo, e é geralmente produzida em uma taxa praticamente constante pelo corpo — taxa diretamente proporcional à massa muscular da pessoa:.'
+)
